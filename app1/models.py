fa02b60bf3bb1749969369ec9feddd82d8b0038d
MOmagian <mirzacoder4477@gmail.com>
2025-02-24 11:12:01 -0600

    updates in models.py

--- app1/models.py
+++ app1/models.py
@@ -11,6 +11,8 @@ class ProcessedPage(models.Model):
     phone_number = models.CharField(max_length=20)  # (XXX)XXX-XXXX format
     pdf_file = models.FileField(upload_to="processed_pdfs/")  # Extracted page
     created_at = models.DateTimeField(auto_now_add=True)
+class Searches(models.Model):
+    count = models.IntegerField(default=0)  # Simple integer field with default value
 
     def __str__(self):
         return f"{self.phone_number} - {self.pdf_file.name}"
